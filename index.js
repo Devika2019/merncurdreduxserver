833df3b015c40212059252bfca80cd50bfd1a36c
@Devika2019 <devikaraj92@gmail.com>
2023-09-25 18:48:07 -0400

    package

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ app.delete('/deleteUser/:id',(req,res)=>{
     .then(response =>res.json(response))
     .catch(err=>res.json(err))
 })
-app.listen(PORT, () =>{
-    console.log("Server is running")
-})
+// app.listen(PORT, () =>{
+//     console.log("Server is running")
+// })
   
   
